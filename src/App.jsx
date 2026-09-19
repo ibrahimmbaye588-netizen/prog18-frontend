3fc8f1ba321158c0429b5ccaa1c02169294e38e3
ibrahimmbaye588-netizen <ibrahimmbaye588@gmail.com>
2026-09-19 23:09:01 +0000

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,12 @@ import Inscription from "./pages/Inscription.jsx";
 import TableauDeBord from "./pages/TableauDeBord.jsx";
 import Catalogue from "./pages/Catalogue.jsx";
 import Devis from "./pages/Devis.jsx";
+import ImpressionDevis from "./pages/ImpressionDevis.jsx";
 import Stock from "./pages/Stock.jsx";
+import Parametres from "./pages/Parametres.jsx";
+import Ventes from "./pages/Ventes.jsx";
+import Clients from "./pages/Clients.jsx";
+import Menu from "./pages/Menu.jsx";
 import { obtenirToken } from "./api.js";
 
 function RouteProtegee({ enfant }) {
@@ -21,6 +26,18 @@ export default function App() {
         element={<RouteProtegee enfant={<TableauDeBord />} />}
       />
       <Route
+        path="/ventes"
+        element={<RouteProtegee enfant={<Ventes />} />}
+      />
+      <Route
+        path="/clients"
+        element={<RouteProtegee enfant={<Clients />} />}
+      />
+      <Route
+        path="/menu"
+        element={<RouteProtegee enfant={<Menu />} />}
+      />
+      <Route
         path="/catalogue"
         element={<RouteProtegee enfant={<Catalogue />} />}
       />
@@ -29,8 +46,16 @@ export default function App() {
         element={<RouteProtegee enfant={<Devis />} />}
       />
       <Route
+        path="/devis/:id/imprimer"
+        element={<RouteProtegee enfant={<ImpressionDevis />} />}
+      />
+      <Route
         path="/stock"
         element={<RouteProtegee enfant={<Stock />} />}
+      />
+      <Route
+        path="/parametres"
+        element={<RouteProtegee enfant={<Parametres />} />}
       />
     </Routes>
   );
